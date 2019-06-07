5f373525dcf227f06dfc9065e9d6dcf79a3f67fa
Canaan Etai <canaan@winexviv.com>
2019-06-07 22:23:18 +0000

    added cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,8 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Cors
-{
+class Cors {
+
     /**
      * Handle an incoming request.
      *
@@ -13,10 +13,12 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next) {
+
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+            ->header('Access-Control-Allow-Credentials',' true');
     }
 }
